7e30b045a988cda0a04904858c52a7db069b1f60
Kurt Griarte <kurtgee11@gmail.com>
2017-08-14 19:49:58 -0400

    update

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,26 +1,5 @@
-console.log("Linked")
-
 $(document).ready(function () {
-    $("#make-reservation").click(function () {
-        event.preventDefault();
-        var resName = $('#reserve_name').val();
-        var resPhone = $('#reserve_phone').val();
-        var resEmail = $('#reserve_email').val();
-        var resObj = {
-            name: resName,
-            phone: resPhone,
-            email: resEmail
-        };
-
-        $.post('http://localhost:3000/recieve-reservation', resObj, function () {
-            console.log("Record sent");
-            if (resObj === true) {
-                alert("reservation done");
-            } else {
-                alert("Sorry you are in wait list");
-            }
-        });
-
-        window.location.href = "tables.html";
+    $("#submitButton").click(function () {
+        console.log("Linked")
     });
 });
